07ea6851c7908f7ccf5afef06f2fcd74fb016a92
“camillebrown” <camillebrown11@gmail.com>
2020-09-15 10:08:28 -0700

    Completed combining_datatypes.js activities

--- combining_datatypes.js
+++ combining_datatypes.js
@@ -77,3 +77,116 @@ const powerButton = () => {
 }
 
 console.log(powerButton()())
+
+///=================================Model a Vending Machine =================================///
+// a vending machine is an object
+
+const vendingMachine = {
+    // it has an array of snacks (make 3 snacks)
+        snacks: [
+    // snacks are objects that have a name and a price        
+            chips = {
+                name: 'Ruffles',
+                price: 5
+            },
+            pickle = {
+                name: 'Dill',
+                price: 3
+            } ,
+            peanuts = {
+                name: 'Cashews',
+                price: 4
+            } 
+        ],
+    // a vending machine has a function vend that allows user to enter the array position (a number) of the snack and then that snack will be returned
+        vend: (number) => {
+            return vendingMachine.snacks[number]
+        }
+    // Be able to call vendingMachine.vend() with a valid integer to return a snack
+}
+    
+vendingMachine.vend(2)
+
+///=================================Callbacks=================================///
+
+// Make a function add that takes two arguments (numbers) and sums them together
+const add = (num1,num2) => {
+    return num1 + num2
+}
+
+add(3,6)
+
+// Make a function subtract that takes two arguments (numbers) and subtracts them
+
+const subtract = (num1,num2) => {
+    return num1 - num2
+}
+
+subtract(15,3)
+// Make a function multiply that takes two arguments and multiplies them
+
+const multiply = (num1,num2) => {
+    return num1 * num2
+}
+
+multiply(10,8)
+// Make a function divide that takes two arguments and divides them
+const divide = (num1,num2) => {
+    return num1 / num2
+}
+
+divide(56,7)
+// Make a function calculate that takes three arguments. Assume the two arguments are a number ie num1, num2 and a function called operates (a callback).
+
+const calculate = (num1,num2,callback) => {
+    return callback(num1,num2);
+}
+  
+// Make it so that when calculate is invoked, the callback "operates" on the numbers and returns the value.
+
+calculate(56,7,multiply)
+// Call calculate 4 times, each time using one of the operation functions you wrote
+calculate(56,7,divide)
+calculate(56,7,subtract)
+calculate(56,7,add)
+
+///=================================Function definition placement=================================///
+
+//Clean up this code, so that it works and has function definitions in the correct place
+//=====OLD/INCORRECT=====//
+bar();
+const bar = () => {
+    console.log('bar here');
+}
+foo();
+
+const foo = () => {
+    console.log('foo here');
+}
+
+//=====NEW/CORRECT=====//
+
+const bar = () => {
+    console.log('bar here');
+}
+bar();
+
+const foo = () => {
+    console.log('foo here');
+}
+foo();
+
+///=================================Error reading=================================///
+
+//What is meant by the error(s) this produces?
+
+foo();
+
+const foo ()=>{
+    console.log('hi');
+}
+
+//Answer: We're missing the initializer (=) that is required in ES6 to begin the function. We should have foo=()=>{ and then the code to run. We also have the function called before it is defined but that has a different error message: "Cannot access 'foo' before initialization"
+
+
+
